e207239f52d1b24fa3e5e13686459554362ff03f
I562818 <sri.sai.lahari.gandrapu@sap.com>
2024-03-15 10:47:57 +0530

    changed the document

--- migration-script/strategy/db-setup/technicalConfig.js
+++ migration-script/strategy/db-setup/technicalConfig.js
@@ -1,28 +1,28 @@
 const shell = require("shelljs");
 const fs1 = require("fs");
-const addDeployFormat = require("../addDeployFormat");
+// const addDeployFormat = require("../addDeployFormat");
 
 const technicalConfig = (directory, option) => {
   try {
 
     //converting to hdbtable-format
-    if(option == 3){
-      var cap_db_dest = directory + "/db";
-      process.chdir(cap_db_dest);
-      const files = shell.find(".").filter((file) => file.endsWith(".cds"));
-      files.forEach((file) => {
-        let fileData = fs1.readFileSync(file, "utf8");
-        let updatedFileData = fileData.replace(/@sql\.append:\s+`{3}\s*technical configuration \{\s*([\s\S]*?)\s*;\s*\}\s*`{3}/g,(match, p1) => {
-        let techConfigValue = p1.trim();
-        return `@sql.append: \`\`\`\n${techConfigValue}\n\`\`\``;
-        });
-      fs1.writeFileSync(file, updatedFileData, "utf8");
-      });   
-      process.chdir("../");
-      addDeployFormat(option)
-    console.log("Condition met, exiting server");
-    process.exit(0)
-    }
+    // if(option == 3){
+    //   var cap_db_dest = directory + "/db";
+    //   process.chdir(cap_db_dest);
+    //   const files = shell.find(".").filter((file) => file.endsWith(".cds"));
+    //   files.forEach((file) => {
+    //     let fileData = fs1.readFileSync(file, "utf8");
+    //     let updatedFileData = fileData.replace(/@sql\.append:\s+`{3}\s*technical configuration \{\s*([\s\S]*?)\s*;\s*\}\s*`{3}/g,(match, p1) => {
+    //     let techConfigValue = p1.trim();
+    //     return `@sql.append: \`\`\`\n${techConfigValue}\n\`\`\``;
+    //     });
+    //   fs1.writeFileSync(file, updatedFileData, "utf8");
+    //   });   
+    //   process.chdir("../");
+    //   addDeployFormat(option)
+    // console.log("Condition met, exiting server");
+    // process.exit(0)
+    // }
 
     const files = shell.find(directory).filter((file) => file.endsWith(".cds"));
     files.forEach(function (file) {
