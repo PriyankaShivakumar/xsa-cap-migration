8e4598735af2ba625ed7675445bb006173587aa5
I562818 <sri.sai.lahari.gandrapu@sap.com>
2024-04-12 21:24:11 +0530

    fixed technical config and annotations

--- migration-script/strategy/db-setup/annotationChanges.js
+++ migration-script/strategy/db-setup/annotationChanges.js
@@ -72,7 +72,7 @@ const removeAnnotation = (directory) => {
       .filter((file) => file.endsWith(".cds"));
     cdsFiles.forEach(function (file) {
       let fileData = fs1.readFileSync(file, "utf8");
-      let regex = /annotation\b\s\w+:\s\w+\(\d+\)\s*;?/g;
+      let regex = /annotation\b\s\w+\s*:\s*\w+\(\d+\)\s*;?/g
       let updatedData = fileData.replace(regex, "");
       fs1.writeFileSync(file, updatedData, "utf8");
     });
