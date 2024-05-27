d790a4aaec52f02e444de6d7841cc3e427374c90
I562818 <sri.sai.lahari.gandrapu@sap.com>
2024-05-27 14:06:34 +0530

    added changes for time()

--- migration-script/renameStrategy/charReplacementDate.js
+++ migration-script/renameStrategy/charReplacementDate.js
@@ -1,11 +1,13 @@
 class CharReplacementDate{   
     configure(config){
         this.regex = new RegExp("date\\(", 'g');
+        this.regexTime = new RegExp("time\\(", 'g');
         this.replacement = "daydate(";
+        this.replacementTime = "secondtime(";
     }
     
     process(fileContent){
-        let updatedContent = fileContent.replace(this.regex, this.replacement);
+        let updatedContent = fileContent.replace(this.regex, this.replacement).replace(this.regexTime, this.replacementTime);
         return updatedContent;
     }
 }
